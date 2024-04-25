0e6418829f2cfa057b0db2e9f4f19861d866c942
wjdwltn <jlsowltn2000@naver.com>
2024-04-25 21:09:44 +0900

    DFS_11724

---
+++ src/DFS_11724.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class DFS_11724 {
+    static int N,M;
+    static int[][] arr;
+    static boolean[] visited;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr = new int[N+1][N+1];
+        visited = new boolean[N+1];
+        for(int i=1; i<=M; i++){
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            arr[x][y] = 1;
+            arr[y][x] = 1;
+        }
+        int count=0;
+        for(int i=1; i<=N; i++){
+            if(!visited[i]){
+                dfs(i);
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+    public static void dfs(int x){
+        if(visited[x]){
+            return;
+        }
+        else{
+            visited[x] = true;
+            for(int i =1; i<=N; i++){
+                if(arr[x][i]==1){
+                    dfs(i);
+                }
+            }
+        }
+    }
+}
